7e7bc65d833aaae29180d040092c6df95cb245fc
Ryan <ryan.dsilva.98@gmail.com>
2018-08-06 21:05:36 +0530

    Updated app.js

--- app.js
+++ app.js
@@ -1,12 +1,58 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const sanitizer = require('express-sanitizer');
+const methodOverride = require('method-override');
+const mongoose = require('mongoose');
+const passport = require('passport');
+const LocalStrategy = require('passport-local');
+
+//Import Routes
+const IndexRoutes = require('./routes/index');
+const UserRoutes = require('./routes/user');
+const CouncilRoutes = require('./routes/council');
+const EventRoutes = require('./routes/event');
+
+//Import Schemas
+const User = require('./models/user');
+const Council = require('./models/council');
+
 const app = express();
 
-const port = (process.env.PORT || 3000);
+//Middleware Setup
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json())
+app.use(sanitizer());
+app.use(methodOverride('_method'));
+app.use(require("express-session")({
+  secret: "Common Council Dashboard",
+  resave: false,
+  saveUninitialized: false
+}));
 
-app.get('/', (req, res) => {
-  res.send('Hi!');
-});
+//Variables
+const port = process.env.PORT || 3000;
+const db = process.env.DB || 'mongodb://localhost/councildashboard';
+const address = process.env.IP || '127.0.0.1'
 
-app.listen(port, () => {
-  console.log('Council Dashboard API listening on port '+ port);
+//Database
+mongoose.connect(db);
+
+//Auth
+app.use(passport.initialize());
+app.use(passport.session());
+//passport.use(new LocalStrategy(User.authenticate()));
+passport.use(new LocalStrategy(Council.authenticate()));
+//passport.serializeUser(User.serializeUser());
+passport.serializeUser(Council.serializeUser());
+//passport.deserializeUser(User.deserializeUser());
+passport.deserializeUser(Council.deserializeUser());
+
+//Routes
+//app.use(IndexRoutes);
+//app.use(UserRoutes);
+//app.use(CouncilRoutes);
+//app.use(EventRoutes);
+
+app.listen(port, address, () => {
+  console.log('Council Dashboard API listening on port ' + port);
 });
---
+++ routes/council.js
@@ -0,0 +1,6 @@
+const express = require('express');
+const Router = express.Router();
+
+Router.get('/council/login', (req, res) => {
+  //Display Login Page
+});
