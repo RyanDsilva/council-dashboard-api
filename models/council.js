aea2aafb7aab7d1b0d2c1f3bf67cc215c1b2e845
Ryan <ryan.dsilva.98@gmail.com>
2018-08-06 20:07:54 +0530

    Added Prettier Config

--- models/council.js
+++ models/council.js
@@ -2,22 +2,22 @@ var mongoose = require('mongoose');
 var passportLocalMongoose = require('passport-local-mongoose');
 
 var CouncilSchema = new mongoose.Schema({
-    name: String,
-    description: String,
-    password: String,
-    isAdmin: Boolean,
-    members: [
-        {
-           type: mongoose.Schema.Types.ObjectId,
-           ref: "User"
-        },
-     ],
-     events: [
-      {
-         type: mongoose.Schema.Types.ObjectId,
-         ref: "Events"
-      },
-   ] 
+  name: String,
+  description: String,
+  password: String,
+  isAdmin: Boolean,
+  members: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User'
+    }
+  ],
+  events: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Events'
+    }
+  ]
 });
 
 CouncilSchema.plugin(passportLocalMongoose);
