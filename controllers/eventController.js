03705d7e20ed080d73f4bc510b9d77faa55372b3
Ryan <ryan.dsilva.98@gmail.com>
2018-08-22 22:34:42 +0530

    Added Event Controller

--- controllers/eventController.js
+++ controllers/eventController.js
@@ -1,50 +1,87 @@
-
 const Event = require('../models/event');
-const Council = require('../models/council');
 const User = require('../models/user');
 
 let eventController = {};
 
-eventController.add = (req,res) => {
-    var event = new Event({ name: req.body.name });
-    event.description = req.body.description;
-
-
-
-};
-
-eventController.edit = (req,res) => {
-
-};
-
-eventController.register = (req,res) => {
-    var event = new Event({ name: req.body.name });
-  event.register=
-  council.isAdmin = false;
-  Council.register(council, req.body.password, (err, council) => {
+eventController.create = (req, res) => {
+  var event = new Event({ name: req.body.name });
+  event.description = req.body.description;
+  event.date = req.body.date;
+  event.image = req.body.image;
+  event.type = req.body.type;
+  event.duration = req.body.duration;
+  event.heldBy = req.user.id;
+  event.registrations = [];
+  event.save(function(err) {
     if (err) {
-      //TODO: Handle the Error
+      console.log(err);
     } else {
-      passport.authenticate('local')(req, res, () => {
-        //TODO: Add Flash Message for Success
-        res.redirect('/council/' + council._id + '/dashboard');
+      res.redirect('/event/all');
+    }
+  });
+};
+
+eventController.search = (req, res) => {
+  Event.findById(req.params.id, (err, event) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.send(event);
+    }
+  });
+};
+
+eventController.fetchAll = (req, res) => {
+  Event.find((err, events) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.send(events);
+    }
+  });
+};
+
+eventController.edit = (req, res) => {
+  Event.findByIdAndUpdate(
+    req.params.id,
+    req.body.event,
+    { new: true },
+    (err, event) => {
+      if (err) {
+        console.log(err);
+      } else {
+        res.send(event);
+      }
+    }
+  );
+};
+
+eventController.register = (req, res) => {
+  Event.findById(req.params.id, (err, event) => {
+    if (err) {
+      console.log(err);
+    } else {
+      User.findById(req.user.id, (err, user) => {
+        if (err) {
+          console.log(err);
+        } else {
+          event.registrations.push(user);
+          event.save();
+          res.send(event);
+        }
       });
     }
   });
-
 };
 
-eventController.displayEvent = (req,res) => {
-
+eventController.delete = (req, res) => {
+  Event.findByIdAndRemove(req.params.id, err => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.redirect('/event/all');
+    }
+  });
 };
 
-eventController.delete = (req,res) => {
-    Event.findByIdAndRemove(req.params.id, err => {
-        if (err) {
-          //TODO: Handle Errors
-        } else {
-          res.redirect('/');
-        }
-      });
-
-};
+module.exports = eventController;
--- routes/event.js
+++ routes/event.js
@@ -1,34 +1,33 @@
 const express = require('express');
 const Router = express.Router();
-const Event = require('../models/event');
 const Controller = require('../controllers/eventController');
 
 Router.post('/event/create', (req, res) => {
-    Controller.create(req, res);
+  Controller.create(req, res);
 });
 
-Router.post('/event/register', (req, res) => {
-    Controller.register(req, res);
+Router.post('/event/:id/register', (req, res) => {
+  Controller.register(req, res);
 });
 
 Router.get('/event/all', (req, res) => {
-    Controller.findAll(req, res);
+  Controller.fetchAll(req, res);
 });
 
 Router.get('/event/:id', (req, res) => {
-    Controller.find(req, res);
+  Controller.search(req, res);
 });
 
-Router.get('/event/edit/:id', (req, res) => {
-    Controller.find(req, res);
+Router.get('/event/:id/edit', (req, res) => {
+  Controller.search(req, res);
 });
 
-Router.put('/event/edit/:id', (req, res) => {
-    Controller.edit(req, res);
+Router.put('/event/:id/edit', (req, res) => {
+  Controller.edit(req, res);
 });
 
-Router.post('/event/delete/:id', (req, res) => {
-    Controller.delete(req, res);
+Router.post('/event/:id/delete', (req, res) => {
+  Controller.delete(req, res);
 });
 
 module.exports = Router;
