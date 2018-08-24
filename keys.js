dc50970f05783daf959ab54ab80f582e704099f0
Ryan <ryan.dsilva.98@gmail.com>
2018-08-24 23:08:10 +0530

    Deploy

---
+++ keys.js
@@ -0,0 +1,10 @@
+keys = {};
+
+keys.GOOGLE_CLIENT_ID =
+  '846047337135-leg0s7jqn8fk4djkvaorg3d4kr1lsjmt.apps.googleusercontent.com';
+keys.GOOGLE_CLIENT_SECRET = 'w3GaOIc7VXMLbLtNSNjlrIN-';
+keys.DATABASE =
+  'mongodb://admin:admin@123@ds259410.mlab.com:59410/councildashboard';
+keys.COOKIE_KEY = 'councildashboardprojectcrce';
+
+module.exports = keys;
