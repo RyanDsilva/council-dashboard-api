b046237a41280d934eb3578cbe014dd74797c596
Ryan <ryan.dsilva.98@gmail.com>
2018-08-24 19:52:09 +0530

    Event controller refactored

--- controllers/eventController.js
+++ controllers/eventController.js
@@ -7,6 +7,8 @@ EventController.create = (req, res) => {
   Event.create({ name: req.body.name }, (err, event) => {
     if (err) {
       console.log(err);
+      req.flash('error', 'An Error Occured! Please try again!');
+      res.redirect('/event/all');
     } else {
       event.description = req.body.description;
       event.date = req.body.date;
@@ -16,7 +18,7 @@ EventController.create = (req, res) => {
       event.heldBy = req.user._id;
       event.registrations = [];
       event.save();
-      //res.send(event);
+      req.flash('Event Created Successflly!');
       res.redirect('/event/all');
     }
   });
@@ -26,6 +28,8 @@ EventController.search = (req, res) => {
   Event.findById(req.params.id, (err, event) => {
     if (err) {
       console.log(err);
+      req.flash('error', 'An Error Occured! Please try again!');
+      res.redirect('/event/all');
     } else {
       res.send(event);
     }
@@ -36,6 +40,8 @@ EventController.fetchAll = (req, res) => {
   Event.find((err, events) => {
     if (err) {
       console.log(err);
+      req.flash('error', 'An Error Occured! Please try again!');
+      res.redirect('/');
     } else {
       res.send(events);
     }
@@ -43,32 +49,35 @@ EventController.fetchAll = (req, res) => {
 };
 
 EventController.edit = (req, res) => {
-  Event.findByIdAndUpdate(
-    req.params.id,
-    req.body.event,
-    { new: true },
-    (err, event) => {
-      if (err) {
-        console.log(err);
-      } else {
-        res.send(event);
-      }
+  Event.findByIdAndUpdate(req.params.id, req.body.event, (err, event) => {
+    if (err) {
+      console.log(err);
+      req.flash('error', 'An Error Occured! Please try again!');
+      res.redirect('/event/' + req.params.id);
+    } else {
+      req.flash('success', 'Event Updated successfully!');
+      res.redirect('/event/' + req.params.id);
     }
-  );
+  });
 };
 
 EventController.register = (req, res) => {
   Event.findById(req.params.id, (err, event) => {
     if (err) {
       console.log(err);
+      req.flash('error', 'An Error Occured! Please try again!');
+      res.redirect('/event/' + req.params.id);
     } else {
       User.findById(req.user.id, (err, user) => {
         if (err) {
           console.log(err);
+          req.flash('error', 'An Error Occured! Please try again!');
+          res.redirect('/event/' + req.params.id);
         } else {
           event.registrations.push(user);
           event.save();
-          res.send(event);
+          req.flash('success', 'Registered successfully!');
+          res.redirect('/event/' + req.params.id);
         }
       });
     }
@@ -79,7 +88,10 @@ EventController.delete = (req, res) => {
   Event.findByIdAndRemove(req.params.id, err => {
     if (err) {
       console.log(err);
+      req.flash('error', 'An Error Occured! Please try again!');
+      res.redirect('/event/' + req.params.id);
     } else {
+      req.flash('success', 'Event Deleted successfully!');
       res.redirect('/event/all');
     }
   });
