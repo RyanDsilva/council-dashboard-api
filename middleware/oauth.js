4791a0316bfdce000dafc86ec069037b618acfca
Ryan <ryan.dsilva.98@gmail.com>
2018-08-24 23:18:19 +0530

    Fix OAuth Callback

--- middleware/oauth.js
+++ middleware/oauth.js
@@ -6,7 +6,7 @@ const GoogleOauth = new GoogleStrategy(
   {
     clientID: process.env.GOOGLE_CLIENT_ID || keys.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET || keys.GOOGLE_CLIENT_SECRET,
-    callbackURL: 'http://localhost:3000/auth/google/callback'
+    callbackURL: 'http://council.netlify.com/auth/google/callback'
   },
   function(accessToken, refreshToken, profile, done) {
     process.nextTick(function() {
