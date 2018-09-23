a8d0ffccff6890b276a700440abd3b63879916ff
Ryan <ryan.dsilva.98@gmail.com>
2018-09-23 17:12:10 +0530

    Fixed Middleware

--- middleware/index.js
+++ middleware/index.js
@@ -2,45 +2,18 @@ const Event = require('../models/event');
 
 let Authorization = {};
 
-Authorization.isAuthenticated = (req, res, next) => {
-  if (req.isAuthenticated()) {
-    return next();
-  }
-  req.flash('error', 'You need to be logged in to do that!');
-  res.redirect('user/login');
-};
-Authorization.isAdmin = (req, res, next) => {
-  if (req.isAuthenticated()) {
-    if (req.user.isAdmin) {
-      return next();
-    } else {
-      req.flash('error', 'You do not have the permission to do that!');
-      res.redirect('back');
-    }
-  } else {
-    req.flash('error', 'You need to be logged in to do that!');
-    res.redirect('council/login');
-  }
-};
 Authorization.isEventOwner = (req, res, next) => {
-  if (req.isAuthenticated()) {
-    Event.findById(req.params.id, (err, event) => {
-      if (err) {
-        req.flash('error', 'An Error Occurred');
-        res.redirect('event/all');
+  Event.findById(req.params.id, (err, event) => {
+    if (err) {
+      res.status(404).json(err);
+    } else {
+      if (event.heldBy.id.equals(req.body.user._id)) {
+        next();
       } else {
-        if (event.heldBy.id.equals(req.user._id)) {
-          next();
-        } else {
-          req.flash('error', 'You do not have permission to do that!');
-          res.redirect('back');
-        }
+        res.status(403).json(err);
       }
-    });
-  } else {
-    req.flash('error', 'You need to be logged in to do that!');
-    res.redirect('council/login');
-  }
+    }
+  });
 };
 
 module.exports = Authorization;
